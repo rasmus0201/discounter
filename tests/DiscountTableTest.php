dc88efcd7c0f729b5ac5ca1e90a3d3dc0795d579
rasmus0201 <xxr@live.dk>
2019-09-24 14:27:45 +0200

    fix test

--- tests/DiscountTableTest.php
+++ tests/DiscountTableTest.php
@@ -10,7 +10,7 @@ class DiscountTableTest extends TestCase
 {
     private $basePrice = 100;
 
-    public function test_get_best_discount_for_percentage_ruleset()
+    public function test_get_table_percentage_ruleset()
     {
         $rules = [
             [
@@ -35,5 +35,49 @@ class DiscountTableTest extends TestCase
         $this->assertCount(2, $table);
         $this->assertEquals(10, $table[0]['amount']);
         $this->assertEquals(15, $table[1]['amount']);
+    }
+
+    public function test_get_table_for_mixed_ruleset()
+    {
+        $rules = [
+            [
+                'operator' => '=',
+                'qty' => 1,
+                'amount' => 10,
+                'type' => 'amount',
+            ],
+            [
+                'operator' => '=',
+                'qty' => 2,
+                'amount' => 20,
+                'type' => 'amount',
+            ],
+            [
+                'operator' => '>=',
+                'qty' => 3,
+                'amount' => 15,
+                'type' => 'percentage',
+            ],
+            [
+                'operator' => '>',
+                'qty' => 5,
+                'amount' => 10,
+                'type' => 'percentage',
+            ],
+        ];
+
+        $discounter = new SelectiveDiscounter();
+        $discountTable = new DiscountTable($discounter);
+
+        $table = $discountTable->get($this->basePrice, $rules);
+
+        $this->assertCount(6, $table);
+
+        $this->assertEquals(10, $table[0]['amount']);
+        $this->assertEquals(20, $table[1]['amount']);
+        $this->assertEquals(15, $table[2]['amount']);
+        $this->assertEquals(15, $table[3]['amount']);
+        $this->assertEquals(15, $table[4]['amount']);
+        $this->assertEquals(15, $table[4]['amount']);
     }
 }
